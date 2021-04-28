db0d23cfd21b889825429cf9133745afdc1036bc
Crispy-rw <nshimyumukizachristian@gmail.com>
2021-04-28 20:20:44 +0000

    fixes

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = str(number)[-1:]
-if int(last_digit) > 5:
-    print("Last digit of {:d} is {} and is greater than 5".format(number, last_digit))
-elif int(last_digit) < 6 and int(last_digit) != 0:
-    print("Last digit of {:d} is {} and is less than 6 and not 0".format(number, int(last_digit) * -1  if number < 0 else number ))
+if number < 0:
+    last = ((number * -1) % 10) * -1
 else:
-    print("Last digit of {:d} is {} and is 0".format(number, last_digit))
-  
+    last = number % 10
+if last > 5:
+    print("Last digit of {} is {} and is greater than 5".format(number, last))
+elif last == 0:
+    print("Last digit of {} is {} and is 0".format(number, last))
+elif last < 6 and last != 0:
+    print("Last digit of {} is {} and is less\
+ than 6 and not 0".format(number, last))
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in ["abcdefghijklmnopqrstuvwxyz"]:
-    if i != "q" and i != "e":
-        print(i, end='')
+for i in range(ord('a'), ord('z')+1):
+    if chr(i) not in "eq":
+        print("{:c}".format(i), end='')

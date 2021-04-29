6a12e0d95b7aaa785d4921434793234f4d5c1bb1
Crispy-rw <nshimyumukizachristian@gmail.com>
2021-04-29 19:41:36 +0000

    fix argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 import sys
+if __name__ == "__main__":
 
-ac = len(sys.argv)
+    ac = len(sys.argv) - 1
 
-if __name__ == '__main__':
-    if ac == 1:
-        print("0 argument.")
-    elif ac == 2:
-        print("{} argument:".format(ac - 1))
-        print("{}: {}".format(ac, sys.argv[1]))
-    else:
-        print("{} arguments:".format(ac - 1))
+    if len(sys.argv) - 1 == 0:
+        print("{} arguments.".format(ac))
+        exit()
+
+    if len(sys.argv) - 1 == 1:
+        print("{} argument:".format(ac))
+        for x in sys.argv[1:]:
+            print("{}: {}".format(ac, x))
+
+    if len(sys.argv) - 1 > 1:
+        print("{} arguments:".format(ac))
         for x, n in enumerate(sys.argv[1:], 1):
             print("{}: {}".format(x, n))

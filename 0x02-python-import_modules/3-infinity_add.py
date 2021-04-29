092fa47aa5033972419c3175522b9e11d488e73f
Crispy-rw <nshimyumukizachristian@gmail.com>
2021-04-29 19:30:56 +0000

    fix

--- 0x02-python-import_modules/3-infinity_add.py
+++ 0x02-python-import_modules/3-infinity_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
-
-newlist = [int(x) for x in sys.argv[1:]]
-
+sums = 0
 if __name__ == '__main__':
-    print("{}".format(sum(newlist)))
+    for v in sys.argv[1:]:
+        sums += int(v)
+    print(sums)

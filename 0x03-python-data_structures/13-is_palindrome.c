e24afe2658659a7441ce6e37d9aaa8b76dacbbf6
Crispy-rw <nshimyumukizachristian@gmail.com>
2021-05-05 19:35:40 +0000

    palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+/**
+ *is_palindrome - check if it is palindrome
+ *@head: Node of the list
+ *Return: int
+ */
+int is_palindrome(listint_t **head)
+{
+	const listint_t *current;
+	int len;
+	int i, j;
+	int arr[10000];
+
+	if (*head == NULL)
+		return (1);
+	current = *head;
+	len = 0;
+	while (current != NULL)
+	{
+		current = current->next;
+		len++;
+	}
+
+	if (len == 1)
+		return (1);
+
+	current = *head;
+	i = 0;
+	while (current != NULL)
+	{
+		arr[i] = current->n;
+		i++;
+		current = current->next;
+	}
+
+	j = 0;
+	i--;
+	len--;
+	while (i >= (len / 2))
+	{
+		if (arr[i] != arr[j])
+			return (0);
+		i--;
+		j++;
+	}
+	return (1);
+}

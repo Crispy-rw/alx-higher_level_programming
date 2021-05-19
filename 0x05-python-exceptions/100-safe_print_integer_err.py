07eb48bb051f139278a808c00ec394ed95a55da6
Crispy-rw <nshimyumukizachristian@gmail.com>
2021-05-19 20:59:24 +0000

    advanced tasks

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    """Prints an integer and handles exceptions"""
+    try:
+        print("{:d}".format(value))
+        return True
+    except:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return False
---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    """Execute functions safely and prints error messages, if any, to stderr"""
+
+    try:
+        return fct(*args)
+    except:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return None

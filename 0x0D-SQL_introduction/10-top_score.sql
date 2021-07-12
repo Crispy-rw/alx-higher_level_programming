e62f19f0161e1ce7228f4ab274fa88040f5ad64a
Crispy-rw <nshimyumukizachristian@gmail.com>
2021-07-12 19:50:19 +0000

    best_score,no cheating

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- MYSQL script that lists all records of the table second_table
+-- of the database hbtn_0c_0 in your MySQL server. ORDER BY score and name
+SELECT score, name FROM second_table ORDER BY score DESC;

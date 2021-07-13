a76a53590f851bc2bb5a4c634b800dadc62410a9
Crispy-rw <nshimyumukizachristian@gmail.com>
2021-07-13 19:38:26 +0000

    create db

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,3 @@
+-- MYSQL script that creates the table force_name on your MySQL server
+-- Description: id INT, name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);

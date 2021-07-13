4eb97f481050e1fa8e54d4bdeaa93eb942d34c42
Crispy-rw <nshimyumukizachristian@gmail.com>
2021-07-13 20:26:21 +0000

    joins

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,3 @@
+-- Import the database dump from hbtn_0d_tvshows to MySQL server
+-- MYSQL script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id WHERE tv_show_genres.show_id IS NULL ORDER BY tv_shows.title, tv_show_genres.genre_id;

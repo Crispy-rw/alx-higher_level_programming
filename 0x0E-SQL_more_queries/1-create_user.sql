38fe2cb079686ecd8775d3536bb2d6522a7848f0
Crispy-rw <nshimyumukizachristian@gmail.com>
2021-07-13 19:36:33 +0000

    create user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- MYSQL script that creates the MySQL server user user_0d_1: should have all
+-- privileges on MySQL server, password should be set to user_0d_1_pwd.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;

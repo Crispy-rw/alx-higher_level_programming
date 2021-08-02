f55ec892aaf47fecf6a7159026b0bcf725233819
Crispy-rw <nshimyumukizachristian@gmail.com>
2021-08-02 20:55:48 +0000

    [ft]: while loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const myStrings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let myIndex = 0;
+
+while (myIndex < 3) {
+  console.log(myStrings[myIndex]);
+  myIndex++;
+}

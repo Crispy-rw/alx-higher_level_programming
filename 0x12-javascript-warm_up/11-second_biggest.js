5c2c25c46d0ad917d0b56ae856e9532a499e625e
Crispy-rw <nshimyumukizachristian@gmail.com>
2021-08-02 22:28:02 +0000

    [ft]: max

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+if (process.argv.length == 1){
+  console.log('0');
+}else{
+  const num = process.argv.slice(2).map(x => parseInt(x));
+  num.sort(function (a, b) { return b - a; });
+  console.log(num[1]);
+}

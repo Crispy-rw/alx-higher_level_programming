8ec556e32c9d2665eba7c43b2a3169324e97e0bb
Crispy-rw <nshimyumukizachristian@gmail.com>
2021-08-02 20:52:24 +0000

    [bg]: parse

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-const myVar = process.argv[2];
-if(isNaN(myVar)){
-console.log('Not a number');
-}else{
-console.log(`My number: ${parseInt(myVar)}`);
+
+const myVar = parseInt(process.argv[2]);
+if (process.argv[2] === undefined || isNaN(myVar)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + myVar);
 }

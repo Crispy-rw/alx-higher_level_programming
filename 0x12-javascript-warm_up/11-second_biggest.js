657e0933895b285dc6cf99bbd9079a8551855ef4
Crispy-rw <nshimyumukizachristian@gmail.com>
2021-08-02 22:34:42 +0000

    [fx]: export

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-if (process.argv.length == 1){
+if (process.argv.length === 1) {
   console.log('0');
-}else{
+} else {
   const num = process.argv.slice(2).map(x => parseInt(x));
   num.sort(function (a, b) { return b - a; });
   console.log(num[1]);

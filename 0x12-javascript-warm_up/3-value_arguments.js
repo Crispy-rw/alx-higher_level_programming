b90806246d013714b9f90a42641690bc64e34d07
Crispy-rw <nshimyumukizachristian@gmail.com>
2021-08-02 20:07:02 +0000

    [ft]: list arguments

---
+++ 0x12-javascript-warm_up/3-value_arguments.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// This script prints the first argument passed to it
+if (process.argv[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
+}

92e3e4611e95fd781f88f6be8c2d18e78e2394e5
Crispy-rw <nshimyumukizachristian@gmail.com>
2021-08-08 20:33:51 +0000

    [fix]: print

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 """
-Prints all City objects from the database hbtn_0e_14_usa
+Using the SQLAlchemy module, list all State objects from the
+database hbtn_0e_6_usa
 """
-if __name__ == '__main__':
-    from model_city import City
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
-    import sys
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import City
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    data = session.query(
-        State, City).filter(State.id == City.state_id).order_by(City.id).all()
-    for state, city in data:
-        print('{}: ({}) {}'.format(state.name, city.id, city.name))
-    session.close()
+
+    for row in session.query(City, State)\
+                      .filter(City.state_id == State.id).order_by(City.id):
+        print("{}: ({}) {}".format(row.State.name, row.City.id, row.City.name))
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
 """
-Class definition of a City
+Define a class State and an instance Base using the sqlalchemy ORM
 """
-from model_state import Base, State
-from sqlalchemy import Column, Integer, String, ForeignKey
+
+import sys
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, Integer
+
+Base = declarative_base()
 
 
 class City(Base):
+    """Definition of class City
+    Parameters:
+        id (int): unique identifier for each city
+        name (string): name of the city
+        state_id (int): ID of state that city is in
+    """
+
     __tablename__ = 'cities'
-    id = Column(Integer, autoincrement=True,
-                unique=True, nullable=False, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)

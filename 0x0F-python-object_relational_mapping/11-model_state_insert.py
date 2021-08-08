5782f129869325847e1e7a01f55674726a3d2a4e
Crispy-rw <nshimyumukizachristian@gmail.com>
2021-08-08 20:14:17 +0000

    bg: fix insert

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 """
-Adds the State object “Louisiana” to the database hbtn_0e_6_usa
+Add the State object 'Louisiana' to the database hbtn_0e_6_usa
+using the SQLAlchemy module
 """
-if __name__ == '__main__':
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
-    import sys
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    add_state = State(name='Louisiana')
-    session.add(add_state)
+
+    session.add(State(name='Louisiana'))
+    print("{}".format(session.query(State.id).filter(State.name == "Louisiana")
+                      .first()[0]))
     session.commit()
-    print(add_state.id)
-    session.close()

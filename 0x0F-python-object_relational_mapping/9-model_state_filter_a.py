0fadf103be8cf490d8ebe353b9a409d779835bdb
Crispy-rw <nshimyumukizachristian@gmail.com>
2021-08-08 20:06:28 +0000

    [bg]: fix ilike

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -3,20 +3,20 @@
 lists all State objects that contain
 the letter a from the database hbtn_0e_6_usa
 """
-if __name__ == '__main__':
-    from model_state import Base, State
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
-    import sys
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    data = session.query(State).filter(State.name.like('%a%')).all()
-    for state in data:
-        print('{}: {}'.format(state.id, state.name))
-    session.close()
+    for row in session.query(State)\
+                      .filter(State.name.ilike('%a%'))\
+                      .order_by(State.id):
+        print("{}: {}".format(row.id, row.name))

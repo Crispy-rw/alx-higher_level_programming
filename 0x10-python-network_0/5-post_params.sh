cedfac543b2db781f22e3569e941f64ccb48e582
Crispy-rw <nshimyumukizachristian@gmail.com>
2021-08-19 19:29:23 +0000

    fix post params

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a POST request to the passed URL, and displays the body of the response
-curl -sX POST "$1" --data "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+# bash script that takes a URL & sends a POST request to the URL
+curl -s "$1" -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
